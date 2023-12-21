80a8972eb7ec6e3fb0315bc2c861401cee45e6b0
Somya Agarwal <somya.agarwal@vast.com>
2023-12-21 13:53:03 +0530

    Content changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -21,7 +21,7 @@ const Navbar = () => {
           <Link
             href='/'
             className='flex z-40 font-semibold'>
-            <span>quill.</span>
+            <span>docswithai.</span>
           </Link>
 
           <MobileNav isAuth={!!user} />
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -16,8 +16,8 @@ export function absoluteUrl(path: string) {
 }
 
 export function constructMetadata({
-  title = "Quill - the SaaS for students",
-  description = "Quill is an open-source software to make chatting to your PDF files easy.",
+  title = "DocsWithAi - the SaaS for students",
+  description = "DocsWithAi is an open-source software to make chatting to your PDF files easy.",
   image = "/thumbnail.png",
   icons = "/favicon.ico",
   noIndex = false
@@ -45,10 +45,10 @@ export function constructMetadata({
       title,
       description,
       images: [image],
-      creator: "@joshtriedcoding"
+      creator: "@coderthatwrites"
     },
     icons,
-    metadataBase: new URL('https://quill-jet.vercel.app'),
+    metadataBase: new URL('https://docswithai.vercel.app'),
     themeColor: '#FFF',
     ...(noIndex && {
       robots: {
